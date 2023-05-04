c4958cdb2b753d892bfa7cc38caa83515ad8d135
Hannes Oswald <hannes.oswald97@gmail.com>
2023-05-04 11:06:52 +0200

    remove unused asyncTaskExecutor

--- src/main/java/org/example/config/LoomConfig.java
+++ src/main/java/org/example/config/LoomConfig.java
@@ -1,21 +1,13 @@
 package org.example.config;
 
-import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.support.TaskExecutorAdapter;
 
 import java.util.concurrent.Executors;
 
 @Configuration
 public class LoomConfig {
-
-    @Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
-    public AsyncTaskExecutor asyncTaskExecutor() {
-        return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
-    }
 
     @Bean
     public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
